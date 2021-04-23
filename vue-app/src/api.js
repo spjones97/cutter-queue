2a8a1179baff310a31b7388429c58b36eadb9089
Spencer <sjones97@email.unc.edu>
2021-04-23 17:40:46 -0400

    fix spelling error

--- vue-app/src/api.js
+++ vue-app/src/api.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-var PORT = pocess.env.PORT || 8081
+var PORT = process.env.PORT || 8081
 
 const client = axios.create({
     baseURL: `http://localhost:${PORT}`,
